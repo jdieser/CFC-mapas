e5d245b333d1ed764fdb54384c337425b73ee005
Cristian Zamar <cristianzamar@gmail.com>
2015-09-25 20:51:52 -0300

    added minimap plugin

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ var map = new L.Map('map').setView([-40, -64], 4);
 //create layers
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 12, attribution: osmAttrib});
-
+var osm = new L.TileLayer(osmUrl, {minZoom: 3, attribution: osmAttrib});
+var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
 var ggl = new L.Google();
 
 var baselayers = {"Satelital Google":ggl,"OpenStreetMap":osm};
@@ -17,5 +17,6 @@ map.addLayer(ggl);
 
 // create controls
 L.control.layers(baselayers,{}).addTo(map);
+L.control.minimap(osm2, { toggleDisplay: true }).addTo(map);
 
 //});
