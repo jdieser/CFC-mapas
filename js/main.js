6fe6cdbe1a9a30cccff364f30fc12364606562e9
Cristian Zamar <cristianzamar@gmail.com>
2015-09-25 19:03:12 -0300

    created OSM layer

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@
 // create map
 var map = new L.Map('map').setView([-40, -64], 4);
 
+//create layers
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 12, attribution: osmAttrib});
+
+
+
 //});
