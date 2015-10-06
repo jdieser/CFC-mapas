a5bb1c6e1cc04e1d9961be27a304cf6a1548b3e6
Cristian Zamar <cristianzamar@gmail.com>
2015-10-06 08:49:19 -0300

    added layer style

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var osm = new L.TileLayer(osmUrl, {minZoom: 3, attribution: osmAttrib});
 var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
 var ggl = new L.Google();
 var baselayers = {"Satelital Google":ggl,"OpenStreetMap":osm};
-
+var prov;
 // add default layer
 map.addLayer(ggl);
 
@@ -18,9 +18,18 @@ map.addLayer(ggl);
 var selector = L.control.layers(baselayers,{}).addTo(map);
 L.control.minimap(osm2, { toggleDisplay: true }).addTo(map);
 
-
+// create geojson layer
 $.getJSON("provincias.json", function(data){ 
-     var prov = L.geoJson(data).addTo(map);
+     prov = L.geoJson(data,{
+           style: {
+                 weight: 1,
+                 opacity: 1,
+                 color: 'white',
+                 dashArray: '3',
+                 fillOpacity: 0
+           },
+           onEachFeature: provEachFeature
+     }).addTo(map);
      
      selector.addOverlay(prov,"Provincias");
 });
