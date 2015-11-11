d1ce30badd3a73b5c110f83f15efb7a8794e3082
Cristian Zamar <cristianzamar@gmail.com>
2015-11-11 09:46:26 -0300

    minor fixes

--- js/main.js
+++ js/main.js
@@ -20,31 +20,31 @@ L.control.minimap(osm2, { toggleDisplay: true }).addTo(map);
 
 // create geojson layer
 $.getJSON("provincias.json", function(data){ 
-     prov = L.geoJson(data,{
-           style: function(feature) {
-              return { 
-                weight: 1,
-                opacity: 1,
-                color: 'white',
-                dashArray: '3',
-                fillOpacity: feature.properties.wms_url? 0.3 : 0
-              }
-           },
-           onEachFeature: provEachFeature
-     }).addTo(map);
-     
-     selector.addOverlay(prov,"Provincias");
+	prov = L.geoJson(data,{
+		style: function(feature) {
+			return { 
+				weight: 1,
+				opacity: 1,
+				color: 'white',
+				dashArray: '3',
+				fillOpacity: feature.properties.wms_url? 0.3 : 0
+			}
+		},
+		onEachFeature: provEachFeature
+	}).addTo(map);
+	
+	selector.addOverlay(prov,"Provincias");
 });
 
 // event functions
 function provEachFeature(feature, layer){
-      layer.on({
-        mouseover: mouseoverHandler,
-        mouseout: mouseoutHandler,
-        click: clickHandler
-      });
-      //add labels
-      layer.bindLabel(feature.properties.provincia);
+	layer.on({
+		mouseover: mouseoverHandler,
+		mouseout: mouseoutHandler,
+		click: clickHandler
+	});
+	  //add labels
+	layer.bindLabel(feature.properties.provincia);
 }
 
 function mouseoverHandler(e) {
@@ -65,8 +65,8 @@ function mouseoutHandler(e) {
 }
 
 function clickHandler(e) {
-  var polygon = e.target;
-  active_prov = polygon.feature;
+	var polygon = e.target;
+	active_prov = polygon.feature;
 	//zoom to feature
 	map.fitBounds(polygon.getBounds());
 	//remove mouseover handler
@@ -75,64 +75,74 @@ function clickHandler(e) {
 	})
 	//set default style
 	prov.resetStyle(polygon);
-  //remove fill color
+	//remove fill color
 	polygon.setStyle({fillOpacity: 0});
 	//remove feature label
 	polygon.unbindLabel();
-  //add layers to sidebar
-  listLayers(active_prov);
-  //
-  $('#url p').text('WMS server: ' + (active_prov.properties.wms_url || '--'));
+	//add layers to sidebar
+	listLayers(active_prov);
+	// show wms url
+	$('#url').text('WMS server: ' + (active_prov.properties.wms_url || '--'));
+	
+	adjustUlBottom();
+}
+
+function adjustUlBottom() {
+	var url_height = parseInt($('#url').css('height')),
+		github_height = parseInt($('#github').css('height')),
+		bottom_value = url_height + github_height + 20;
+
+	$('.sidebar-nav').css('bottom', bottom_value);
 }
 
 function listLayers(provincia) {
-  //remove current list
-  $('.layer').remove();
+	//remove current list
+	$('.layer').remove();
 
-  if (provincia.properties.wms_url) {
-    //loading message
-    $('#abstract').css('display', 'block');
-    $('#abstract p').text('Cargando capas...');
-    //get wms layers 
-    loadLayerList(provincia.properties.wms_url);
+	if (provincia.properties.wms_url) {
+		//loading message
+		$('#abstract').css('display', 'block');
+		$('#abstract p').text('Cargando capas...');
+		//get wms layers 
+		loadLayerList(provincia.properties.wms_url);
 
-  } else {
-    //no wms message
-    $('#abstract').css('display', 'block');
-    $('#abstract p').text('No se encontró un servicio WMS para el Catastro de ' 
-      + provincia.properties.provincia);
-  } 
+	} else {
+		//no wms message
+		$('#abstract').css('display', 'block');
+		$('#abstract p').text('No se encontró un servicio WMS para el Catastro de ' 
+			+ provincia.properties.provincia);
+	} 
 }
 
 //adds layer list to sidebar
 function loadLayerList(wms_url) {
-    var url = "wms_getlayers.php?url=" + wms_url;
-    //gets layer list from server
-    $.getJSON( url, function(data) {
-        //hide abstract
-        $('#abstract').css('display', 'none');
+	var url = "wms_getlayers.php?url=" + wms_url;
+	//gets layer list from server
+	$.getJSON( url, function(data) {
+		//hide abstract
+		$('#abstract').css('display', 'none');
 
-        //add layers to ul
-        $.each(data, function(i, item) {
-          $('ul.sidebar-nav').append(
-            '<li class=\'layer\' data-layername=\'' + item.name + '\'><a href="#">' + item.title + '</a></li>'
-          );
-        });
+		//add layers to ul
+		$.each(data, function(i, item) {
+		  $('ul.sidebar-nav').append(
+			'<li class=\'layer\' data-layername=\'' + item.name + '\'><a href="#">' + item.title + '</a></li>'
+		  );
+		});
 
-        setLiClickHandlers();
-    });
+		setLiClickHandlers();
+	});
 }
 
 function setLiClickHandlers() {
-    //adds layer when layer list item is clicked
-    $('.layer').click(function() {
-        var newLayer = L.tileLayer.wms( active_prov.properties.wms_url, {
-            layers: $(this).data("layername"),
-            format: 'image/png',
-            transparent: true
-        }).addTo(map);
+	//adds layer when layer list item is clicked
+	$('.layer').click(function() {
+		var newLayer = L.tileLayer.wms( active_prov.properties.wms_url, {
+			layers: $(this).data("layername"),
+			format: 'image/png',
+			transparent: true
+		}).addTo(map);
 
-        selector.addOverlay(newLayer, $(this).text() + ' (' + active_prov.properties.provincia + ')');
-    });
+		selector.addOverlay(newLayer, $(this).text() + ' (' + active_prov.properties.provincia + ')');
+	});
 }
 //});
